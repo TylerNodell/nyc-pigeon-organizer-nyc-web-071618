a5933ff7111cd60391d8e950d5a0ef2ee1ff11c8
Tyler Nodell <tyler.l.nodell@gmail.com>
2018-07-15 10:48:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |category, options|
+    options.each do |details, name_list|
+      name_list.each do |name|
+          pigeon_list[name] ||= {}
+          pigeon_list[name][category] ||= []
+          pigeon_list[name][category] << details.to_s
+      end
+    end
+  end
+pigeon_list
 end
